f75f63654b0e4cee276181e0007846aeeb63f67a
Sajeel Hassan <hsajeel.dev@gmail.com>
2021-05-12 10:28:41 +0500

    Assignment submitted

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isShow, setisShow] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setisShow(false);
+  };
+  const showFormHandler = () => {
+    setisShow(true);
+  };
+  const hideFormHandler = () => {
+    setisShow(false);
   };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {!isShow && <button onClick={showFormHandler}>Add Expense</button>}
+      {isShow && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          hideForm={hideFormHandler}
+        />
+      )}
     </div>
   );
 };
